1543a6cd892be501a378eb3f286ec70a8dd0dc31
cargabsj175 <cargabsj175@gmail.com>
2021-10-17 14:51:17 -0400

    first email implementation

--- include/myintbox_snippets.php
+++ include/myintbox_snippets.php
@@ -21,3 +21,39 @@ function wpb_woo_my_account_order() {
 	return $myorder;
 }
 add_filter ( 'woocommerce_account_menu_items', 'wpb_woo_my_account_order' );
+
+/**
+ * Correo posterior al registro de usuario.
+ */
+
+add_filter( 'wp_new_user_notification_email' , 'edit_user_notification_email', 10, 3 );
+function edit_user_notification_email( $wp_new_user_notification_email, $user, $blogname ) {
+	global $wpdb;	
+     // Llamamos a las configuraciones del panel
+$my_international_mailbox_options = get_option( 'my_international_mailbox_option_name' ); // Array of All Options
+$your_company_0 = $my_international_mailbox_options['your_company_0']; // Your Company
+$address_mailbox_1 = $my_international_mailbox_options['address_mailbox_1']; // Address Mailbox
+$aditional_info_2 = $my_international_mailbox_options['aditional_info_2']; // Aditional Info
+// obtener valores de usuario
+global $current_user;
+get_currentuserinfo();
+    $message = sprintf(__( "¡Bienvenido a %s! Aquí tienes tus datos de acceso:" ), $blogname ) . "\r\n";
+	$message .= __('Url de acceso: ');
+    $message .= wp_login_url() . "\r\n";
+    $message .= sprintf(__( 'Usuario: %s' ), $user->user_login ) . "\r\n";
+	$message .= sprintf(__( 'E-mail: %s' ), $user->user_email) . "\r\n\r\n";  
+	$message .= __('Datos del buzón:') . "\r\n";
+    $message .= sprintf(__( 'Dirección del buzón: %s' ), $my_international_mailbox_options['address_mailbox_1'] ) . "\r\n";
+    $message .= sprintf(__( 'ID de buzón de Usuario: %s' ), $user->random_number ) . "\r\n\r\n";
+	
+	$message .= __('Para establecer tu contraseña dirigete a: ');
+	$message .= wp_lostpassword_url( home_url() ) . "\r\n";  
+	
+    $message .= sprintf(__( 'Si tiene algún problema o duda, por favor contáctenos a %s.'), get_option( 'admin_email' ) ) . "\r\n\r\n";
+    $message .= __('Copyright © 2021 LatinChina Group. Todos los derechos reservados.');
+
+    $wp_new_user_notification_email['message'] = $message;
+
+    return $wp_new_user_notification_email;
+
+}
